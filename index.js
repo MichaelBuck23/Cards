5002bb58acb03becbdd807697d8863321f39cfee
Michael Buck <michael.buck23@gmail.com>
2015-02-05 15:07:45 -0400

    Added cool ascii faces. They're awesome.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var ascii = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+  response.send(ascii());
 });
 
 app.listen(app.get('port'), function() {
